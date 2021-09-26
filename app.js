183fdd07fa2b1556d840a6e308c34246a6bdee90
Your Name <cuonglinkhousemientrung@gmail.com>
2021-09-26 11:03:09 +0700

    make it better

---
+++ app.js
@@ -0,0 +1,126 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import request from "sync-request";
+import cache from "memory-cache";
+import { url } from "inspector";
+
+//variable
+const port = process.env.PORT || 3100;
+
+//init
+const app = express();
+app.use(express.static("./public"));
+app.set("view engine", "ejs");
+app.set("views", "./views");
+app.get("/", (req, res) => {
+  res.render("trangchu");
+});
+const server = createServer(app);
+const io = new Server(server, {});
+server.listen(port);
+
+//socket
+io.on("connection", (socket) => {
+  console.log(`${socket.id} connected`);
+  // cacheInit(socket);
+
+  socket.on("disconnect", () => {
+    console.log(`${socket.id} disconnected`);
+  });
+
+  socket.on("Client-send-data", (data) => {
+    socket.emit("Server-send-data", changeContent(data));
+  });
+});
+
+// function cacheInit(socket){
+//   const cacheData = cache.get('publisher');
+//   if(cacheData){
+//     console.log("có dữ liệu:"+cacheData.api_token);
+//     socket.emit("Server-send-cache", cacheData);
+//   }else{
+//     console.log("đã thêm mới dữ liệu");
+//     cache.put('publisher',{"api_token": "1234s"});
+//   }
+// }
+
+function changeContent(data) {
+  const exp = /(https?:\/\/[^\s]+)/g;
+  
+  let offers ;
+  try{
+    offers = JSON.parse(request('POST','https://pub.masoffer.com/api/extension/info',
+  {
+    headers: {
+      Authorization: data.api_token,
+      "Content-Type": "application/json",
+    },
+    json: {token:data.api_token},
+    
+  }).getBody()).data.offers;
+  }catch{
+    return "Token error"
+  }
+  
+
+  
+
+  let dataOutput = data.data.replace(exp, (e) => {
+    let oldUrl;
+    try {
+      oldUrl = new URL(request("GET", e).url);
+    } catch (error) {
+      return "ERROR_Link_nhu_sh*t: "+ e;
+    }
+    
+    const offersOfUrls = offers.filter(offer => offer.domain.includes(oldUrl.hostname));
+    let bestOffersOfUrl;
+    if(offersOfUrls.length!=0){
+      bestOffersOfUrl = offersOfUrls[offersOfUrls.length-1].offer_id;
+    }else{
+      return "DOMAIN_NAY_CHUA_DUOC_DANG_KY"+ oldUrl;
+    }
+    
+
+    let linkData = {
+      publisher_id: data.userId,
+      domain: "rutgon.me",
+      offer_type: "cps",
+      params: {
+        offer_id: bestOffersOfUrl,
+        url: oldUrl.origin + oldUrl.pathname + oldUrl.search, //"https://shopee.vn/product/329422701/9905688095?af_click_lookback=7d&af_reengagement_window=7d&af_siteid=an_17194710000&af_sub_siteid=614eaadf9cbab90043a2df8f----eXQ_NSw1r9etBVSM_kSAVQ&af_viewthrough_lookback=1d&c=-&d_id=34b8d&is_retargeting=true&pid=affiliates&smtt=0.12008485-1629892077.9&utm_campaign=-&utm_content=614eaadf9cbab90043a2df8f----eXQ_NSw1r9etBVSM_kSAVQ&utm_medium=affiliates&utm_source=an_17194710000",
+        aff_sub1: "toolVip",
+        aff_sub2: "",
+        aff_sub3: "",
+        aff_sub4: data.data.split(' ').slice(0,3).join('_'),
+      },
+    };
+
+    const options = {
+      headers: {
+        Authorization: data.api_token,
+        "Content-Type": "application/json",
+      },
+      json: linkData,
+    };
+
+  
+   try {
+    const returnData = JSON.parse(
+      request(
+        "POST",
+        "https://pub.masoffer.com/api/extension/generate-link",
+        options
+      ).getBody()
+    );
+    const newUrl = returnData.data.short_url;
+    return newUrl;
+   } catch (error) {
+    return "ERROR_Link_chua_chuyen: " + e;
+   }
+   
+  });
+  // console.log(dataOutput);
+  return dataOutput;
+}
---
+++ public/socketapp.js
@@ -0,0 +1,75 @@
+$(document).ready(() => {
+  const socket = io("/");
+  if ($.cookie("userId")) {
+    showDataFromCookie();
+  } else {
+    console.log("chuwa ton tai cookie");
+  }
+
+  $("#btnLuu").click(() => {
+    const value = $("#api_token").val();
+    if (value) {
+      $("#alert").html(`Đang kiểm tra ...`);
+      requestInfo(value);
+    }else
+    {
+      showDataFromCookie();
+    }
+  });
+
+  socket.on("Server-send-data", (data) => {
+    $("#noidung").html("Thành công...");
+    $("#taOutput").html(data);
+  });
+
+  //   socket.on("Server-send-cache", (data) => {
+  //     $("#api_token").val(data.api_token);
+  //     console.log(data.api_token);
+  //   });
+
+  $("#mrA").click(() => {
+    const dataInput = $("#taInput").val();
+    if (dataInput) {
+      $("#noidung").html("Đang ấy ấy chờ xíu nhé...");
+      socket.emit("Client-send-data", {
+        data: dataInput,
+        api_token: $.cookie("api_token"),
+        userId: $.cookie("userId"),
+        userName: $.cookie("userName"),
+      });
+    }else{
+      $("#noidung").html("Chưa nhập nội dung kìa cha nội...");
+    }
+  });
+
+  function requestInfo(token) {
+    $.get({
+      url: "https://pub.masoffer.com/api/extension/info",
+      data: {
+        token: token,
+      },
+      success: (response) => {
+        if (response.status == 200) {
+          let now = new Date().toLocaleString();
+
+          $.cookie("api_token", token);
+          $.cookie("userName", response.data.user.name);
+          $.cookie("userId", response.data.user.refcode);
+          $.cookie("last_fetch", now);
+
+          showDataFromCookie();
+        } else {
+          console.log("error1");
+        }
+      },
+      error: () => {
+        console.log("error2");
+      },
+    });
+  }
+
+  function showDataFromCookie() {
+    $("#api_token").val($.cookie("api_token"));
+    $("#alert").html(`${$.cookie("userId")} => ${$.cookie("userName")} `);
+  }
+});
