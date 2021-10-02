d1b9e2802c8fd58f4e71ebc4a89ef77d94278834
Your Name <cuonglinkhousemientrung@gmail.com>
2021-10-02 17:49:23 +0700

    á

--- public/socketapp.js
+++ public/socketapp.js
@@ -252,17 +252,17 @@ $(document).ready(() => {
     const tilehoahong = 0.6;
   
 
-    $('#hoaHongChoDuyet').html(thongKe.hoaHongChoDuyet*tilehoahong.toLocaleString("vi",{ maximumFractionDigits: 0 }));
+    $('#hoaHongChoDuyet').html((thongKe.hoaHongChoDuyet*tilehoahong).toLocaleString("vi",{ maximumFractionDigits: 0 }));
     $('#soDonHangChoDuyet').html(thongKe.soDonHangChoDuyet);
-    $('#tongGiaTriChoDuyet').html(thongKe.tongGiaTriChoDuyet*tilehoahong.toLocaleString("vi",{ maximumFractionDigits: 0 })) ;
+    $('#tongGiaTriChoDuyet').html((thongKe.tongGiaTriChoDuyet*tilehoahong).toLocaleString("vi",{ maximumFractionDigits: 0 })) ;
    
-    $('#hoaHongThanhCong').html(thongKe.hoaHongThanhCong*tilehoahong.toLocaleString("vi",{ maximumFractionDigits: 0 }));
+    $('#hoaHongThanhCong').html((thongKe.hoaHongThanhCong*tilehoahong).toLocaleString("vi",{ maximumFractionDigits: 0 }));
     $('#soDonHangThanhCong').html(thongKe.soDonHangThanhCong);
-    $('#tongGiaTriThanhCong').html(thongKe.tongGiaTriThanhCong*tilehoahong.toLocaleString("vi",{ maximumFractionDigits: 0 })) ;
+    $('#tongGiaTriThanhCong').html((thongKe.tongGiaTriThanhCong*tilehoahong).toLocaleString("vi",{ maximumFractionDigits: 0 })) ;
    
-    $('#hoaHongHuy').html(thongKe.hoaHongHuy*tilehoahong.toLocaleString("vi",{ maximumFractionDigits: 0 }));
+    $('#hoaHongHuy').html((thongKe.hoaHongHuy*tilehoahong).toLocaleString("vi",{ maximumFractionDigits: 0 }));
     $('#soDonHangHuy').html(thongKe.soDonHangHuy);
-    $('#tongGiaTriHuy').html(thongKe.tongGiaTriHuy*tilehoahong.toLocaleString("vi",{ maximumFractionDigits: 0 })) ;
+    $('#tongGiaTriHuy').html((thongKe.tongGiaTriHuy*tilehoahong).toLocaleString("vi",{ maximumFractionDigits: 0 })) ;
     $("#divThongKe").show(1000);
   }
 
